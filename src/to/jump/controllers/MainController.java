1b0be7ff1319cce6ecc273b7b0361aa546c9c8b9
Rafael Telles <raftel.ti@gmail.com>
2015-08-06 14:57:11 -0300

    Adicionando rota /search

--- src/to/jump/controllers/MainController.java
+++ src/to/jump/controllers/MainController.java
@@ -1,13 +1,22 @@
 package to.jump.controllers;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import to.jump.dao.LinkDAO;
 
 @Controller
 public class MainController {
-
+	
+	@Autowired
+	LinkDAO linkDao;
+	
 	@RequestMapping("/")
 	public String index(HttpSession session) {
 		if (session.getAttribute("user") != null) {
@@ -22,5 +31,13 @@ public class MainController {
 			return "redirect:/";
 		}
 		return "dashboard";
+	}
+	
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	public String search(String q,
+			HttpServletResponse httpServletResponse, Model model) {
+		model.addAttribute("results", linkDao.searchLinks(q));
+
+		return "search-results";
 	}
 }
