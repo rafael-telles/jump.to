a1ffa0cca511c7d42640bd71b4883bd4c382f792
Rafael Telles <raftel.ti@gmail.com>
2015-08-09 16:09:47 -0300

    Arrumando configurações de persistencia e script para criar as tabelas

--- src/to/jump/utils/CreateTables.java
+++ src/to/jump/utils/CreateTables.java
@@ -4,8 +4,6 @@ import javax.persistence.Persistence;
 
 public class CreateTables {
 	public static void main(String[] args) {
-		Persistence.createEntityManagerFactory("users");
-		Persistence.createEntityManagerFactory("links");
-		Persistence.createEntityManagerFactory("clicks");
+		Persistence.createEntityManagerFactory("jumpto");
 	}
 }
